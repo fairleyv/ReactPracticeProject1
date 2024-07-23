e836b01b093220a710dee48e179a015f43bdf5fa
fairleyv <fairleyvincent@gmail.com>
2024-07-23 12:59:21 -0700

    added css to MainContent and included background image in css of react logo. (Followed Scrimba suggestions. Will refactor to look better on page)

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 export default function Main () {
     return (
+        <>
         <div className="main">
-            <h1>Fun facts about React</h1>
-            <ui className="list-items">
-                <li>Was first released in 2013</li>
-                <li>Was originally creted by Jordan Walke</li>
-                <li>Has well over 100K stars on GitHub</li>
-                <li>Is maintained by Meta</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ui>
+                <h1>Fun facts about React</h1>
+                <ui className="list-items">
+                    <li>Was first released in 2013</li>
+                    <li>Was originally creted by Jordan Walke</li>
+                    <li>Has well over 100K stars on GitHub</li>
+                    <li>Is maintained by Meta</li>
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </ui>
         </div>
+        </>
     )
 }
